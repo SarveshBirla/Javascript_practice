542f0325206024c3e840962bc94cdbd4e2c31ad1
Sarvesh <sab2112003@gmail.com>
2024-01-28 20:22:07 +0530

    learned variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+console.log("Sarvesh");
+
+const id=13324;
+let email="sab2112003@gmail.com"
+var password="122344"
+
+city="Jalgaon";
+
+let name;// undefined 
+
+
+// id = 41234  -> not allowed to change const variable
+console.log(id);
+email="sarvesh.birla@spit.ac.in"
+password="1233345"
+
+/*Prefer using let over var
+   beacause var has block scope and functional scope issues.
+   
+   var has global scope .
+*/
+console.table([id,email,password,city,name]);
---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"// treat all JS code as newer version
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3
+    +3
+    ) //code readability should be high
+    
+    
+let name="Sarvesh";
+let age = 18;
+let isLogged=false
+
+
+//number=> 2 ki power 53
+//bigint => more big numbers
+//string=""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof null)// object
+
+console.log(typeof undefined)// undefined
