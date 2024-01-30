1dc43b6bd62d23b91745f0c226df3441cd8553dd
Sarvesh <sab2112003@gmail.com>
2024-01-30 17:40:46 +0530

    Conversion and comparison of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,52 @@
+let score = "33"
+
+console.log(typeof(score));  // string
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+
+
+// "33"=> 33
+// "33abc"=> NaN (Not a Number)
+// true=>1 ; false ->0
+// null =>0
+// undefined => NaN
+
+
+let isLoggedIn = true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+// 1=> true; 0=> false
+//"" => false
+// "Sarvesh"=>true
+
+let someNumber=33;
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+
+
+
+
+/** ************NumberOperations **************/
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+str1="Sarvesh"
+str2="birla"
+
+str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1" + 2 + 2)
+console.log(1+2+"2")
+
+
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+console.log("2">1)
+console.log("02">1);
+
+console.log(null>0)
+console.log(null == 0)
+console.log(null>=0)
+
+// operators other than == && === consider null as 0
+console.log(undefined>0)
+console.log(undefined == 0)
+console.log(undefined>=0)
+
+// undefined is treated as NaN
+
+
+
+// ===
+
+console.log("2"=== 2)
